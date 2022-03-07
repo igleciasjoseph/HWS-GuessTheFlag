3656b685341896f48dda0b23014f714d1108d5a7
Joseph Iglecias <igleciasjoseph@gmail.com>
2022-03-07 12:36:59 -0500

    first commit

---
+++ HWS-GuessTheFlag/HWS_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_GuessTheFlagApp.swift
+//  HWS-GuessTheFlag
+//
+//  Created by Joseph Iglecias on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct HWS_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
